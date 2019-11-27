3183304fcbb0101013a538c63fa0875ebd1c7969
Glitch (schooltimernew) <none>
2019-11-27 23:07:47 +0000

    schedule abbreviations

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+var schedule = [];
+var scheduleAbb = [];
+
 function ask() {
   var howManyDays = prompt("How many different schedules are at your school?");
   
-  for (let step = 1; step <= howManyDays; step++) {
-    
+  for (var i = 1; i <= howManyDays; ++i) {
+    schedule[i] = prompt("What is the name of schedule day " + i + "?");
   }
   
+   for (var i = 0; i < howManyDays; ++i) {
+    scheduleAbb[i] = prompt("What an abbreviation for schedule " + schedule[i] + "?");
+  }
+
   
   var howManyPeriods = prompt("How many periods are there in a normal schedule?");
   window.location.href = "https://schooltimernew.glitch.me/?howManyPeriods=" + howManyPeriods;
@@ -16,3 +23,21 @@ function read() {
   var c = url.searchParams.get("howManyPeriods");
   alert(c);
 }
+
+
+
+
+
+
+
+
+/*
+
+
+var markers = [];
+for (var i = 0; i < coords.length; ++i) {
+    markers[i] = "some stuff";
+}
+
+
+*/
