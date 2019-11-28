eecc03b723f89d95fe2dfcea02a0d7ffe4cee1a8
Glitch (schooltimernew) <none>
2019-11-28 00:51:12 +0000

    need to jump around functions

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ var schedule = [];
 var scheduleAbb = [];
 var daysURL = "";
 
+var samePURL = "";
+var diffP = [];
+var sameP = [];
+
+var d1 = [];
+var d2 = [];
+var d3 = [];
+var d4 = [];
+var d5 = [];
+var d6 = [];
+
 function ask() {
   var howManyDays = prompt("How many different schedules are at your school?");
    window.location.href = "https://schooltimernew.glitch.me/?d=" + howManyDays;
@@ -17,7 +28,42 @@ function ask() {
    for (var i = 1; i <= howManyDays; ++i) {
      daysURL = daysURL + "d" + i + "=" + schedule[i] + "&" + "d" + i + "a" + "=" + scheduleAbb[i] + "&";
    }
-  alert(daysURL);
+  
+  var sameperiods = prompt("Is there the same number of periods everyday? (yes or no)");
+  if (sameperiods == "no") {
+
+  } else if (sameperiods == "yes") {
+    var numPeriod = prompt("How many periods are there in a day?");
+    
+   
+      
+      for (var e = 1; e <= numPeriod; ++e) {
+          d1[e]= prompt("What hour does period " + e + " end at on a "+ schedule[1] + " day?");
+      }
+     for (var e = 1; e <= numPeriod; ++e) {
+          d2[e]= prompt("What hour does period " + e + " end at on a "+ schedule[2] + " day?");
+      }
+     for (var e = 1; e <= numPeriod; ++e) {
+          d3[e]= prompt("What hour does period " + e + " end at on a "+ schedule[3] + " day?");
+      }
+     for (var e = 1; e <= numPeriod; ++e) {
+          d4[e]= prompt("What hour does period " + e + " end at on a "+ schedule[4] + " day?");
+      }
+     for (var e = 1; e <= numPeriod; ++e) {
+          d5[e]= prompt("What hour does period " + e + " end at on a "+ schedule[5] + " day?");
+      }
+     for (var e = 1; e <= numPeriod; ++e) {
+          d6[e]= prompt("What hour does period " + e + " end at on a "+ schedule[6] + " day?");
+      }
+      
+    
+    alert(d1 + "\n" + d2 + "\n" + d3 + "\n" + d4 + "\n" + d5 + "\n" + d6);
+    
+  } else {
+    alert("why are you typing anything other than yes or no like you were told?");
+    ask();
+  }
+  
   
    window.location.href = "https://schooltimernew.glitch.me/?" + daysURL;
   
@@ -29,21 +75,3 @@ function read() {
   var c = url.searchParams.get("d2");
   alert(c);
 }
-
-
-
-
-
-
-
-
-/*
-
-
-var markers = [];
-for (var i = 0; i < coords.length; ++i) {
-    markers[i] = "some stuff";
-}
-
-
-*/
