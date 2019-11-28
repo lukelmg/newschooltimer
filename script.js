879dbb3dd0ddcac0ff762abac0a24b90444e8017
Glitch (schooltimernew) <none>
2019-11-28 00:13:18 +0000

    can read days

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var schedule = [];
 var scheduleAbb = [];
+var daysURL = "";
 
 function ask() {
   var howManyDays = prompt("How many different schedules are at your school?");
+   window.location.href = "https://schooltimernew.glitch.me/?d=" + howManyDays;
   
   for (var i = 1; i <= howManyDays; ++i) {
     schedule[i] = prompt("What is the name of schedule day " + i + "? \n \n Ex. Normal, Homeroom, etc.");
@@ -11,14 +13,20 @@ function ask() {
    for (var i = 1; i <= howManyDays; ++i) {
     scheduleAbb[i] = prompt("What an abbreviation for schedule " + schedule[i] + "?");
   }
-
-  window.location.href = "https://schooltimernew.glitch.me/?d=";
+  
+   for (var i = 1; i <= howManyDays; ++i) {
+     daysURL = daysURL + "d" + i + "=" + schedule[i] + "&" + "d" + i + "a" + "=" + scheduleAbb[i] + "&";
+   }
+  alert(daysURL);
+  
+   window.location.href = "https://schooltimernew.glitch.me/?" + daysURL;
+  
 }
 
 function read() {
   var url_string = window.location.href;
   var url = new URL(url_string);
-  var c = url.searchParams.get("howManyPeriods");
+  var c = url.searchParams.get("d2");
   alert(c);
 }
 
