a53b993186295f701d1c251d64fa948f955b138c
lukelmg <lukegutman14@gmail.com>
2020-01-19 15:20:03 +0000

    working phpmyadmin databae for short urls

---
+++ database/input.php
@@ -0,0 +1,40 @@
+<?php
+$username = filter_input(INPUT_POST, 'longurl');
+$password = filter_input(INPUT_POST, 'shorturl');
+if (!empty($username)){
+if (!empty($password)){
+$host = "localhost";
+$dbusername = "website";
+$dbpassword = "";
+$dbname = "SchoolTimer";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO urlshortener (longurl, shorturl)
+values ('$username','$password')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
+?>
