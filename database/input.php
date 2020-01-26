f0652c874ee80bb3cd1adcad2b7e413a100c0f1f
lukelmg <lukegutman14@gmail.com>
2020-01-26 16:52:21 +0000

    form for urlshortener working

--- database/input.php
+++ database/input.php
@@ -1,46 +1,29 @@
 <?php
-$username = filter_input(INPUT_POST, 'longurl');
-$password = filter_input(INPUT_POST, 'shorturl');
-if (!empty($username)){
-if (!empty($password)){
-$host = "localhost";
-$dbusername = "website";
-$dbpassword = "";
+$servername = "localhost";
+$username = "website";
+$password = "";
 $dbname = "SchoolTimer";
+
+$uniqueid = filter_input(INPUT_POST, 'uniqueid');
+$timeanddate = filter_input(INPUT_POST, 'timeanddate');
+$longurl = filter_input(INPUT_POST, 'longurl');
+$shorturl = filter_input(INPUT_POST, 'shorturl');
+
 // Create connection
-$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-$check=mysqli_query($conn,"select * from clients where firstname='$firstname' and lastname='$lastname'");
-$checkrows=mysqli_num_rows($check);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
+$sql = "INSERT INTO urlshortener (uniqueid, timeanddate, longurl, shorturl)
+VALUES ('$uniqueid', '$timeanddate', '$longurl', '$shorturl')";
 
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 
-if (mysqli_connect_error()){
-die('Connect Error ('. mysqli_connect_errno() .') '
-. mysqli_connect_error());
-}
-else{
-
-
-
-$sql = "INSERT INTO urlshortener (longurl, shorturl)
-values ('$username','$password')";
-if ($conn->query($sql)){
-echo "New record is inserted sucessfully";
-}
-else{
-echo "Error: ". $sql ."
-". $conn->error;
-}
-$conn->close();
-}
-}
-else{
-echo "Password should not be empty";
-die();
-}
-}
-else{
-echo "Username should not be empty";
-die();
-}
+mysqli_close($conn);
 ?>
