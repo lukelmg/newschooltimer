74b270ce2161dc3354a1cb5858bd4f829e6ecc9d
Luke <lukegutman14@gmail.com>
2020-03-15 17:41:05 +0000

    remixgit add .!

---
+++ remix.js
@@ -0,0 +1,7 @@
+function openEditModal () {
+  document.getElementById("editModal").style.display = "block";
+}
+
+function closeEditModal () {
+  document.getElementById("editModal").style.display = "none";
+}
